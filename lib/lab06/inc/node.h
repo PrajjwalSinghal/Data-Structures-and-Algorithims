428a0cf3355e78e88a7821a30dab329574a54337
prajjwal <singhalprajjwal@gmail.com>
2017-10-19 15:10:40 -0700

    First Push

--- lib/lab06/inc/node.h
+++ lib/lab06/inc/node.h
@@ -1,13 +1,15 @@
 #ifndef UNTITLED1_NODE_H
 #define UNTITLED1_NODE_H
+#include<iostream>
 
+using namespace std;
 // Yes, this is a very tiny node. Everything is accessibly publicly, without accessors or mutators
 
 class node {
 public:
     node *next, *prev;
     unsigned data;
-    explicit node(unsigned int data) : data(data), next(nullptr), prev(nullptr) {}
+    explicit node(unsigned int data) : data(data), next(NULL), prev(NULL) {}
 };
 
 
