f50a727371519f998d469b89f1923112a97ed9cf
prajjwal <singhalprajjwal@gmail.com>
2017-10-19 15:15:56 -0700

    Second Push

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include "lib/lab06/inc/deck.h"
-
+#include "doubly_linked_list.h"
+using namespace std;
 int main() {
     //deck newdeck;
+    vector <unsigned> values(5);
+    values[0] = 4;
+    values[1] = 4;
+    values[2] = 6;
+    values[3] = 6;
+    values[4] = 6;
+    doubly_linked_list d1(values);
+    //cout<<d1.get_size();
+    doubly_linked_list d2(d1);
+    d2.print();
     return 0;
 }
