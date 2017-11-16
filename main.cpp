735d36fec78c29024a4563284d902e22ca525f28
prajjwal <singhalprajjwal@gmail.com>
2017-11-16 14:52:19 -0800

    TEMP COMMIT

--- main.cpp
+++ main.cpp
@@ -1,17 +1,25 @@
 #include "lib/lab06/inc/deck.h"
-#include "doubly_linked_list.h"
+//#include "doubly_linked_list.h"
+#include "lib/lab07/inc/tree.h"
+#include<iostream>
+
 using namespace std;
 int main() {
+
     //deck newdeck;
+    /*
     vector <unsigned> values(5);
-    values[0] = 4;
-    values[1] = 4;
-    values[2] = 6;
-    values[3] = 6;
-    values[4] = 6;
+    values[0] = 0;
+    values[1] = 1;
+    values[2] = 2;
+    values[3] = 3;
+    values[4] = 4;
     doubly_linked_list d1(values);
-    //cout<<d1.get_size();
-    doubly_linked_list d2(d1);
-    d2.print();
+    vector <unsigned> values_temp(d1.get_set(3,4));
+    d1.print();
+    */
+    tree T1;
+    //T1.print();
     return 0;
 }
+
